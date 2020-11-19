e84865c5b9f5e19af89d44c61e0171a05caa1bee
Xitong Gao <gxtfmx@gmail.com>
2020-11-19 08:00:40 +0000

    Note

--- convert.py
+++ convert.py
@@ -5,7 +5,7 @@ import numpy as np
 
 device = torch.device('cuda')
 epsilon = 0.031
-epsilon -= np.finfo(np.float32(1.0)).eps
+# epsilon -= np.finfo(np.float32(1.0)).eps
 x = np.load('data_attack/cifar10_X.npy')
 x = torch.tensor(x, device=device)
 xadv = torch.load(sys.argv[1], map_location=device)['adv_complete']
